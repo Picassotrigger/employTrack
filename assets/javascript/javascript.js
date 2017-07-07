8a3e97f2f154ca6e8c089fe7c0da533e6ce6138d
David DuChene <davidduchene@Davids-MacBook-Pro.local>
2017-07-07 10:29:46 -0500

    Organizing code. Sections for database, variables, functions, and main.

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -1,3 +1,4 @@
+//==========================   DATABASE   ===============================//
  var config = {
     apiKey: "AIzaSyATOZfqjena5ceI9a6Lh4rpUd5SlJIJ-Tk",
     authDomain: "chart-1d648.firebaseapp.com",
@@ -12,17 +13,45 @@
 
 
 
+
+//==========================   VARIABLES   ===============================//
+
+  var monthsEmployed = 0;
+
+  var totalBill = 0;
+
+
+
+
+//==========================   FUNCTIONS   ===============================//
+
+function calcMonthsEmployed(start) {
+  var months = Math.floor(moment(new Date()).diff(moment(date,"MM/DD/YYYY"),'months',true));
+  monthsEmployed = months;
+}
+
+function totalBilling(rate) {
+  var total = monthsEmployed * rate;
+  totalBill = total;
+}
+
+
+
+
+
+
+//==========================   MAIN   ===============================//
 if(database.exists){
 
       for (var i = 0; i < database.numChildren(); i++) {
-       
+
         //put code here to append and prepend the info. already on the server into the html
-       
+
       }
 
       console.log(database);
 
-         
+
          console.log("stuff is here");
       }
 
@@ -50,7 +79,7 @@ database.ref().on("value", function(snapshot) {
         role:role,
         start:start,
         rate:rate,
-        // dateAdded:database.serverValue.TIMESTAMP 
+        // dateAdded:database.serverValue.TIMESTAMP
 
 
 
@@ -89,61 +118,5 @@ $(document).ready(function() {
     $("#resultsBox").DataTable({
     	data: data
     });
-    
+
 } );
-
-
-
-
-//checking to see if there is data there
-
-
-
-
-	//this is for user input
-	
-
-  
-$("button").on("click", function(){
-
-
-
-	var searchTerm=$("#searchInput").val();
-	var yearBegin=$("#startYear").val();
-	var yearEnd=$("#endYear").val();
-	// var yearBeginCode=yearBegin+=0101
-	// var yearEndCode=yearEnd+=1230
-});
-
-
-
-
-//adding stuff to the form in real time if it changed on the server
-
- 
-
-
-
-
-
-<<<<<<< HEAD
-  // var dateStart = start;
-  var monthsEmployed = 0;
-
-  function calcMonthsEmployed(start) {
-    var months = Math.floor(moment(new Date()).diff(moment(date,"MM/DD/YYYY"),'months',true));
-    monthsEmployed = months;
-  }
-
-  // var monthlyRate = 10000;
-  var totalBill = 0;
-
-  function totalBilling(rate) {
-    var total = monthsEmployed * rate;
-    totalBill = total;
-  }
-=======
-  // function calcMonthsEmployed() {
-  //   // moment([]).diff(moment([2013, 9, 31]), 'months', true)
-  // }
->>>>>>> a3df8ca33085a6f108e181e3996cb99d781193e2
